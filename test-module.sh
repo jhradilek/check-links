b7be25f00e97320cf4bbdac128268654c6af169e
Jaromir Hradilek <jhradilek@gmail.com>
2019-04-02 19:55:38 +0200

    Created an outline for a module test script.
    This is heavily based on my check-links script for DocBook XML files
    from 2013.

---
+++ test-module.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+# test-module.sh - test an AsciiDoc module and report possible issues
+# Copyright (C) 2013, 2014, 2019 Jaromir Hradilek <jhradilek@gmail.com>
+
+# This program is  free software:  you can redistribute it and/or modify it
+# under  the terms  of the  GNU General Public License  as published by the
+# Free Software Foundation, version 3 of the License.
+#
+# This program  is  distributed  in the hope  that it will  be useful,  but
+# WITHOUT  ANY WARRANTY;  without  even the implied  warranty of MERCHANTA-
+# BILITY  or  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+# License for more details.
+#
+# You should have received a copy of the  GNU General Public License  along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+# General information about the script:
+declare -r NAME=${0##*/}
+declare -r VERSION='0.0.1'
+
+# Prints an error message to standard error output and terminates the
+# script with a selected exit status.
+#
+# Usage: exit_with_error ERROR_MESSAGE [EXIT_STATUS]
+function exit_with_error {
+  local -r error_message=${1:-'An unexpected error has occurred.'}
+  local -r exit_status=${2:-1}
+
+  # Print the supplied message to standard error output:
+  echo -e "$NAME: $error_message" >&2
+
+  # Terminate the script with the selected exit status:
+  exit $exit_status
+}
+
+# Prints a warning message to standard error output.
+#
+# Usage: warn WARNING_MESSAGE
+function warn {
+  local -r warning_message="$1"
+
+  # Print the supplied message to standard error output:
+  echo -e "$NAME: $warning_message" >&2
+}
+
+# Reads an AsciiDoc module, removes unwanted content such as comments from
+# it, and prints the result to standard output.
+#
+# Usage: print_module FILE
+function print_module {
+  local -r filename="$1"
+
+  # Remove both single-line and multi-line comments from the supplied file:
+  perl -0pe 's{^////\s*\n.*?^////\s*\n}{}msg;s{^//\s.*\n}{}gm;' "$filename"
+}
+
+# Processes the supplied AsciiDoc module and reports problems to standard
+# output.
+#
+# Usage: print_report FILE
+function print_report {
+  local -r filename="$1"
+
+  # TBD
+}
+
+# Process command-line options:
+while getopts ':hv' OPTION; do
+  case "$OPTION" in
+    h)
+      # Print usage information to standard output:
+      echo "Usage: $NAME FILE"
+      echo -e "       $NAME -hv\n"
+      echo '  -h           display this help and exit'
+      echo '  -v           display version and exit'
+
+      # Terminate the script:
+      exit 0
+      ;;
+    v)
+      # Print version information to standard output:
+      echo "$NAME $VERSION"
+
+      # Terminate the script:
+      exit 0
+      ;;
+    *)
+      # Report invalid option and terminate the script:
+      exit_with_error "Invalid option -- '$OPTARG'" 22
+      ;;
+  esac
+done
+
+# Shift positional parameters:
+shift $(($OPTIND - 1))
+
+# Verify the number of command line arguments:
+[[ "$#" -eq 1 ]] || exit_with_error 'Invalid number of arguments' 22
+
+# Get the name of the AsciiDoc file:
+declare -r file="$1"
+
+# Verify that the supplied file exists and is readable:
+[[ -e "$file" ]] || exit_with_error "$file: No such file or directory" 2
+[[ -r "$file" ]] || exit_with_error "$file: Permission denied" 13
+[[ -f "$file" ]] || exit_with_error "$file: Not a file" 21
+
+# Terminate the script:
+exit 0
